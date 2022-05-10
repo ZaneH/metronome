d464ea18dfa17538e3d5c37ee191c1641543c92d
Zane Helton <zane99@me.com>
2022-05-10 18:33:05 -0400

    Clean up isPlaying state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { appWindow, LogicalSize } from '@tauri-apps/api/window'
-import { useState } from 'react'
 import './App.css'
 import KeyboardHandler from './components/KeyboardHandler'
 import { MetroContextProvider } from './components/MetroContextProvider'
 import Metronome from './components/Metronome'
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false)
-
   appWindow.setSize(new LogicalSize(350, 500))
   appWindow.setMinSize(new LogicalSize(350, 500))
 
   return (
     <div className='App'>
-      <MetroContextProvider isPlaying={isPlaying} setIsPlaying={setIsPlaying}>
+      <MetroContextProvider>
         <KeyboardHandler />
         <Metronome />
       </MetroContextProvider>
--- src/components/MetroContextProvider/MetroContextProvider.tsx
+++ src/components/MetroContextProvider/MetroContextProvider.tsx
@@ -3,17 +3,31 @@ import { createContext, Dispatch, FC, SetStateAction, useState } from 'react'
 type MetroContextType = {
   children?: React.ReactNode
 
+  /**
+   * BPM of the metronome
+   */
   bpm?: number
+
+  /**
+   * Is the metronome playing?
+   */
   isPlaying?: boolean
 
+  /**
+   * Set the BPM (used for state updates)
+   */
   setBpm?: Dispatch<SetStateAction<number>>
+
+  /**
+   * Set if playing (used for state updates)
+   */
   setIsPlaying?: Dispatch<SetStateAction<boolean>>
 }
 
 export const MetroContext = createContext({} as MetroContextType)
 
 const MetroContextProvider: FC<MetroContextType> = ({ children }) => {
-  const [bpm, setBpm] = useState(144)
+  const [bpm, setBpm] = useState(120)
   const [isPlaying, setIsPlaying] = useState(false)
 
   const context: MetroContextType = {
