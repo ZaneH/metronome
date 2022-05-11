92b6a33f9bb4e16c9a84e12cfbd4200dd56276ae
Zane Helton <zane99@me.com>
2022-05-11 04:20:33 -0400

    Add Cmd+Q functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Menu, MenuItem, Submenu};
 use tauri_plugin_store::PluginBuilder;
 
 fn main() {
+  let menu = Menu::new().add_submenu(Submenu::new(
+    "Metronome",
+    Menu::new().add_native_item(MenuItem::Quit),
+  ));
+
   tauri::Builder::default()
+    .menu(menu)
     .plugin(PluginBuilder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
