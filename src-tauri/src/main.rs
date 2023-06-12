52b7b9bd32d1749d6b00506816dfefd41c1b286b
Zane Helton <me@zane.pw>
2023-06-12 01:23:07 -0400

    Set allowlist to false, update Tuari packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,20 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use tauri::{Menu, MenuItem, Submenu};
-use tauri_plugin_store::PluginBuilder;
+use tauri_plugin_store;
 
 fn main() {
-  let menu = Menu::new().add_submenu(Submenu::new(
-    "Metronome",
-    Menu::new().add_native_item(MenuItem::Quit),
-  ));
+    let menu = Menu::new().add_submenu(Submenu::new(
+        "Metronome",
+        Menu::new().add_native_item(MenuItem::Quit),
+    ));
 
-  tauri::Builder::default()
-    .menu(menu)
-    .plugin(PluginBuilder::default().build())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .menu(menu)
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
